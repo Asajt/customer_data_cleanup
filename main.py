99ad2cd00549ecee4dcaeec7653d2f8156237a79
Tjasa Grabnar <tjasagrabnar2000@gmail.com>
2025-03-09 12:32:45 +0100

    flow handling

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import pandas as pd
-from utils.create_customer_dataset import load_data, save_data
+from utils.customer_data_generator import generate_synthetic_customers
+from utils.chaos_engineering import apply_errors
 from detection.detect_address import detect_address_errors
 from correction.correct_address import correct_address_errors
+import pandas as pd
+import numpy as np
 
-# Load dataset
-df = load_data("raw_data/customer_data_with_errors.xlsx")
+
+GURS_file_path = 'src/raw_data/RN_SLO_NASLOVI_register_naslovov_20240929.csv'
+output_file_path = 'src/raw_data/customer_data.xlsx'
+
+########################################################################################
+# 01. create customer datsaset
+customer_df = generate_synthetic_customers(file_path = GURS_file_path, dataset_size=5000, output_file_path=output_file_path, seed=42)
+########################################################################################
+
+
+########################################################################################
+# 02. introduce errors into the dataset
+#   Ensure all columns are handled as strings
+customer_df = customer_df.astype(str)
+#    Add a column to track introduced errors
+customer_df["introduced_errors"] = ""
+#   Apply errors
+customer_df_w_errors = apply_errors(customer_df)
+########################################################################################
+
+
+# Save the final dataset
+customer_df.to_excel("src/processed_data/customer_data_with_errors.xlsx", index=False)
+print("Corrupted dataset saved successfully.")
+
+
 
 # Address Error Detection
 df_detected = detect_address_errors(df)
