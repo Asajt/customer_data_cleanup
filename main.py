c39f80063f469f28df256364ecf0e3a0f6aff1ed
Tjasa Grabnar <tjasagrabnar2000@gmail.com>
2025-04-29 20:48:48 +0200

    main file modifications

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from utils.customer_data_generator import generate_synthetic_customer_data
 from utils.chaos_engineering import apply_errors
 
 GURS_file_path = 'src/raw_data/RN_SLO_NASLOVI_register_naslovov_20240929.csv'
-
-path_to_gurs = 'src/raw_data/RN_SLO_NASLOVI_register_naslovov_20240929.csv'
 dataset_size = 10000
 seed = 42
+
+# generate synthetic customer data
 df = generate_synthetic_customer_data(GURS_file_path, dataset_size, seed)
     
-    # introduce errors into the dataset
+# introduce errors into the dataset
 df = apply_errors(df, seed)
 
+# Run the full quality pipeline
 run_full_quality_pipeline(df, 
                               first_name_column="FIRST_NAME", 
                               last_name_column="LAST_NAME", 
@@ -22,4 +23,7 @@ run_full_quality_pipeline(df,
                               email_column="EMAIL", 
                               phone_column="PHONE_NUMBER")
 
-    
+# Save the processed data to an Excel file
+output_file_path = 'src/processed_data/final_customer_data.xlsx'
+df.to_excel(output_file_path, index=False)
+print(f"Processed data saved to {output_file_path}")
